a37697e8aec5cc30fd04bb93f6e88abf7348d0d4
Kevindresbar <kevindresbar@gmail.com>
2026-04-27 21:49:07 -0500

    Suma Prices

--- Ejerciciosclase/Clase27-04/01-fake-api.js
+++ Ejerciciosclase/Clase27-04/01-fake-api.js
@@ -1,9 +1,24 @@
 const API = `https://fakeapi.net/products`;
 
-async function getProductos (){
-const response = await fetch( API );
-const data = await response.json();
+async function getProductos() {
 
+    try {
+        const response = await fetch(API);
+        const data = await response.json();
+       console.log(`El total de los precios es ${total(data)}`);
+       console.log("los datos son", data);
 
-console.log(data);}
-getProductos ();
+    }
+    catch (error) {
+        console.error("Error al conectarse al fake API");
+    }
+}
+function total(precio) {
+     total = 0
+    for (let i = 0; i <precio.data.length; i++) {
+        total += precio.data[i].price;
+    }
+    return total;
+}
+
+getProductos();

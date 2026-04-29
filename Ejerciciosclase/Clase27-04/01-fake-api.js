3f6e790a880cac3e55b831e5b980c533d8ee3148
Kevindresbar <kevindresbar@gmail.com>
2026-04-28 19:51:23 -0500

    Precio*Stock

--- Ejerciciosclase/Clase27-04/01-fake-api.js
+++ Ejerciciosclase/Clase27-04/01-fake-api.js
@@ -7,6 +7,7 @@ async function getProductos() {
         const data = await response.json();
        console.log(`El total de los precios es ${total(data)}`);
        console.log("los datos son", data);
+       console.log (`${stockxprice(data)}`);
 
     }
     catch (error) {
@@ -19,6 +20,18 @@ function total(precio) {
         total += precio.data[i].price;
     }
     return total;
+}
+function stockxprice (stock){
+    let total2 =1;
+    for (let i = 0; i < stock.data.length; i++) {
+
+        const producto = stock.data[i].title;
+        const total2 = stock.data[i].price * stock.data[i].stock;
+
+        console.log(`${producto} el precio del producto por la cantidad de stock  ${total2}`);
+        
+    }
+
 }
 
 getProductos();

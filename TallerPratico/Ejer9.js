aa6804fd56aecf3c2077ff109d01421daf669088
Kevindresbar <kevindresbar@gmail.com>
2026-04-30 19:10:23 -0500

    Taller Practico noveno ejercicio

---
+++ TallerPratico/Ejer9.js
@@ -0,0 +1,6 @@
+let color1 =prompt("Ingrese el primer color ")
+let color2 =prompt("Ingrese el segundo color ")
+let color3 =prompt("Ingrese el tercer color ")
+let colores = []
+colores.push(color1, color2, color3)
+console.log(colores)

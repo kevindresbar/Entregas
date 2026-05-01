3da52d4a3e5d16bef2fdeac1c98ac9498875df71
Kevindresbar <kevindresbar@gmail.com>
2026-04-30 19:22:17 -0500

    Taller Practico ejercicio 12

---
+++ TallerPratico/Ejer12.js
@@ -0,0 +1,9 @@
+ persona = {
+    nombre:"Kevin andres baez",
+    edad: 21,
+    ciudad: "bogota",
+};
+
+persona.ciudad = "Medellin"
+console.log( persona.nombre, persona.edad, persona.ciudad)
+console.log(persona[`nombre`],persona[`edad`],persona[`ciudad`])

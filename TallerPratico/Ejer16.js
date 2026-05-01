123158395a92ff39684eed10af1358fed364b5c0
Kevindresbar <kevindresbar@gmail.com>
2026-04-30 19:42:00 -0500

    Taller practico ejercicio #16

---
+++ TallerPratico/Ejer16.js
@@ -0,0 +1,12 @@
+const num1 = prompt("ingresa el primer numero")
+const num2 = prompt("ingresa el segundo numero")
+if(num1 == num2){
+    console.log("los dos numeros son iguales")
+}
+else if (num1 < num2)
+{
+    console.log("el segundo valor es el mayor")
+}
+else{
+    console.log("el primer valor es el positivo ")
+}

11071c46b1d4e999e2ed47876593104d6a215f53
Kevindresbar <kevindresbar@gmail.com>
2026-05-06 20:31:13 -0500

    Taller Practico Funciones Ejercicio #6

--- TallerPracticoFunciones1/Ejer6.js
+++ TallerPracticoFunciones1/Ejer6.js
@@ -1,15 +1,15 @@
-const estudiante = [{ nombre: "Juan", notaFinal: [2.1, 5.0, 4.2, 3.0] },];
+const estudiante = [ {nombre: "Juan", notas:[2.1, 5, 5.0, 4.2, 3.0]}];
 function promedioNotas(prom) {
     let suma = 0
 for (let i = 0; i < prom.length; i++) {
     
-    for (let j = 0; j < prom[i].notaFinal.length; j++) {
-        let nota = prom[i].notaFinal[j]
+    for (let j = 0; j < prom[i].notas.length; j++) {
+        let nota = prom[i].notas[j]
         suma += nota
         
     }
-    let prome = suma / prom[i].length
+return suma / prom[i].notas.length;
+}
 
 }
-retor}
 console.log(promedioNotas(estudiante))

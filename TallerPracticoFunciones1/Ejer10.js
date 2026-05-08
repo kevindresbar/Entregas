9c9c0aeda29ab1b9416951c0cee449bcb5e712b0
Kevindresbar <kevindresbar@gmail.com>
2026-05-07 21:12:11 -0500

    Taller Practico Funciones Ejercicio #10

---
+++ TallerPracticoFunciones1/Ejer10.js
@@ -0,0 +1,10 @@
+let objeto= {a: 1, b:2, c:3, d:4}
+list =Object.entries(objeto)
+ function contarPropiedades(objeto){
+    suma = 0
+    for (let i = 0; i < objeto.length; i++) {
+    suma ++
+    }
+    return suma
+ }
+ console.log(contarPropiedades(list))

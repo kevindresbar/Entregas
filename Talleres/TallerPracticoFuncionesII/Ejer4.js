49ace8d35866ff3a87025dd170d06cf561519c02
Kevindresbar <kevindresbar@gmail.com>
2026-05-18 18:52:28 -0500

    Taller Practico Funciones II ejercicio #4

---
+++ Talleres/TallerPracticoFuncionesII/Ejer4.js
@@ -0,0 +1,14 @@
+function contarVocales(frase) {
+    let texto = frase.toLowerCase();
+    let conteo = { a: 0, e: 0, i: 0, o: 0, u: 0 };
+
+    for (let i = 0; i < texto.length; i++) {
+        let letra = texto[i];
+        if (conteo[letra] !== undefined) {
+            conteo[letra]++;
+        }
+    }
+    return conteo;
+}
+
+console.log(contarVocales("Hola mundo"));

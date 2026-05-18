ef8aa0efdb07e63098bfa04f1a24f782c3fd6ecb
Kevindresbar <kevindresbar@gmail.com>
2026-05-18 17:14:36 -0500

    Taller Practico Funciones Ejercicio #17

---
+++ TallerPracticoFunciones1/Ejer17.js
@@ -0,0 +1,9 @@
+let numero = prompt("Que tabla desea?")
+
+function tablaMultiplicar(n) {
+    for (let i = 0; i <= 10; i++) {
+        console.log(`${n} x ${i} = ${n * i}`);
+    }
+}
+
+tablaMultiplicar(numero);

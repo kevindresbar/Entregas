4526271590435c10823500dba9bb7edd0a9c83d2
Kevindresbar <kevindresbar@gmail.com>
2026-05-18 16:49:27 -0500

    Taller Practico Funciones Ejercicio #15

---
+++ TallerPracticoFunciones1/Ejer15.js
@@ -0,0 +1,27 @@
+let contra = prompt("Ingrese la contraseña ")
+
+function validarContraseña(contraseña){
+    
+    for (let i = 0; i < contraseña.length; i++) {
+
+
+      if (contraseña.length < 8) {
+        alert("La contraseña debe tener al menos 8 caracteres.");
+        return false;
+    }
+            if (contraseña === contraseña.toLowerCase()) {
+        alert("La contraseña debe tener al menos una mayúscula.");
+        return false;
+    }
+
+
+ if (contraseña === contraseña.toUpperCase()) {
+        alert("La contraseña debe tener al menos una minúscula.");
+        return false;
+    }
+
+alert("¡Contraseña válida!");
+    return true;
+}
+}
+validarContraseña(contra)
